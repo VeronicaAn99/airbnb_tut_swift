79df1baf831d21c9b25fe95252d8b5faafe6dd71
nicaan <nica.an23@gmail.com>
2024-09-15 19:23:21 +0300

    Initial commit

---
+++ AirbnbTutorial/App/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Nica An on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
